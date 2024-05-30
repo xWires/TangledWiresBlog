642452b945282f4f7ec9c9359410905ff17d9aac
xWires <xwires@tangledwires.xyz>
2024-05-30 22:59:49 +0100

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,10 @@
+module.exports = {
+	title: "TangledWires Blog",
+	url: "https://blog.tangledwires.xyz/",
+	language: "en",
+	description: "My blog about software I am working on.",
+	author: {
+		name: "TangledWires",
+		url: "https://tangledwires.xyz/"
+	}
+}
